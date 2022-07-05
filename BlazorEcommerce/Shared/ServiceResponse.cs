424420abce611e9c8d52a8e68ff09a1fa2bd46de
Phillip Drennan <pdrennan@pangotechnology.com>
2022-07-05 14:20:39 -0800

    ProjectDetails Page now shows correctly

--- BlazorEcommerce/Shared/ServiceResponse.cs
+++ BlazorEcommerce/Shared/ServiceResponse.cs
@@ -11,7 +11,7 @@ namespace BlazorEcommerce.Shared
 
         public T? Data { get; set; }
 
-        public bool Success { get; set; }
+        public bool Success { get; set; } = true;
 
         public string Message { get; set; }
     }
