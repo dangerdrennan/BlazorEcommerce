d2fdfd2311b5571dadcb7d2aaff59b2abbbcf58f
Phillip Drennan <pdrennan@pangotechnology.com>
2022-07-06 09:25:28 -0800

    got product pages for each category tab showing

--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -10,20 +10,33 @@
         }
         public List<Product> Products { get; set; } = new List<Product>();
 
+        public event Action ProductsChanged;
+
         public async Task<ServiceResponse<Product>> GetProduct(int productId)
         {
             var res = await _http.GetFromJsonAsync<ServiceResponse<Product>>($"api/product/{productId}");
             return res;
         }
 
-        public async Task GetProducts()
+        //public async Task GetProducts()
+        //{
+        //    var res = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
+        //    if (res != null && res.Data != null)
+        //    {
+        //        Products = res.Data;
+        //    }
+            
+        //}
+
+        public async Task GetProducts(string? categoryUrl = null)
         {
-            var res = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
+            var res = categoryUrl == null ?  await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product") :
+                await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/category/{categoryUrl}");
             if (res != null && res.Data != null)
             {
                 Products = res.Data;
             }
-            
+            ProductsChanged.Invoke();
         }
     }
 }
