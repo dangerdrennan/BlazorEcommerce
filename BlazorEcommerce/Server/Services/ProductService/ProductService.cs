dfd0f11fffbbe50bdad5a555fb65d622f8a03016
Phillip Drennan <pdrennan@pangotechnology.com>
2022-07-06 10:30:25 -0800

    Successful backend call for new tables

--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -12,7 +12,10 @@
         public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
         {
             var res = new ServiceResponse<Product>();
-            var product = await _context.Products.FindAsync(productId);
+            var product = await _context.Products
+                .Include(p => p.Variants)
+                .ThenInclude(v => v.ProductType)
+                .FirstOrDefaultAsync(p=>p.Id==productId);
             if (product == null)
             {
                 res.Success = false;
@@ -30,7 +33,7 @@
         {
             var res = new ServiceResponse<List<Product>>
             {
-                Data = await _context.Products.ToListAsync()
+                Data = await _context.Products.Include(p=>p.Variants).ToListAsync()
             };
             return res;
         }
@@ -40,7 +43,9 @@
             var response = new ServiceResponse<List<Product>>
             {
                 Data = await _context.Products
-                .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower())).ToListAsync()
+                    .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+                    .Include(p => p.Variants)
+                    .ToListAsync()
             };
 
             return response;
